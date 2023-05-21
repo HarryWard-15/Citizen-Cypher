3287da7657d5cdecc0919df6ea3276e9ea2d3941
Harry_Desktop <hward.1508@gmail.com>
2023-05-22 01:39:24 +0800

    now all synced to cloud database instead of local

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from datetime import datetime
 import connection.sqlconnector as sqlconnector
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://be09806f63f8e8:f184440b@us-cdbr-east-06.cleardb.net/heroku_0046abaa65d13c4?reconnect=true'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://be09806f63f8e8:f184440b@us-cdbr-east-06.cleardb.net/heroku_0046abaa65d13c4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hellmadsecret'
 
 app.app_context().push()
 
 db = SQLAlchemy(app)
+
 # sqlconnector.initialise_db(db)
 
 import routes.routes as routes
