d04b419b994877ff64f4806063c1ccff3eecb411
Harry_Desktop <hward.1508@gmail.com>
2023-05-22 01:02:48 +0800

    commenting out crossover code

--- connection/sqlconnector.py
+++ connection/sqlconnector.py
@@ -13,90 +13,90 @@ cnx = mysql.connector.connect(
 
 
 # Initialise database and required tables, seed sample data.
-def initialise_db(db_obj):
-    my_cursor = cnx.cursor()
-    my_cursor.execute("DROP DATABASE IF EXISTS citizencypher")
-    my_cursor.execute("CREATE DATABASE citizencypher")
-    my_cursor.execute("USE citizencypher")
+# def initialise_db(db_obj):
+#     my_cursor = cnx.cursor()
+#     my_cursor.execute("DROP DATABASE IF EXISTS citizencypher")
+#     my_cursor.execute("CREATE DATABASE citizencypher")
+#     my_cursor.execute("USE citizencypher")
 
-    # Classes for user info, previous game data and current game data
-    class User(db_obj.Model):
-        __tablename__ = "user"
+#     # Classes for user info, previous game data and current game data
+#     class User(db_obj.Model):
+#         __tablename__ = "user"
 
-        id = db_obj.Column(db_obj.Integer, primary_key=True)
-        realname = db_obj.Column(db_obj.String(50), nullable=False)
-        email = db_obj.Column(db_obj.String(100), nullable=False, unique=True)
-        password = db_obj.Column(db_obj.String(128), nullable=False)
+#         id = db_obj.Column(db_obj.Integer, primary_key=True)
+#         realname = db_obj.Column(db_obj.String(50), nullable=False)
+#         email = db_obj.Column(db_obj.String(100), nullable=False, unique=True)
+#         password = db_obj.Column(db_obj.String(128), nullable=False)
 
-        previous_games = relationship("PreviousGame", backref="user")
+#         previous_games = relationship("PreviousGame", backref="user")
 
-        def __repr__(self):
-            return "<Name %r>" % self.realname
+#         def __repr__(self):
+#             return "<Name %r>" % self.realname
 
-    class PreviousGame(db_obj.Model):
-        __tablename__ = "previous_game"
+#     class PreviousGame(db_obj.Model):
+#         __tablename__ = "previous_game"
 
-        id = db_obj.Column(db_obj.Integer, primary_key=True, nullable=False)
-        userId = db_obj.Column(db_obj.Integer, ForeignKey("user.id"), nullable=False)
-        causeOfDeath = db_obj.Column(db_obj.String(64), nullable=False)
-        daysSurvived = db_obj.Column(db_obj.Integer, nullable=False)
+#         id = db_obj.Column(db_obj.Integer, primary_key=True, nullable=False)
+#         userId = db_obj.Column(db_obj.Integer, ForeignKey("user.id"), nullable=False)
+#         causeOfDeath = db_obj.Column(db_obj.String(64), nullable=False)
+#         daysSurvived = db_obj.Column(db_obj.Integer, nullable=False)
 
-        def __repr__(self):
-            return "<PreviousGames %r>" % self.daysSurvived
+#         def __repr__(self):
+#             return "<PreviousGames %r>" % self.daysSurvived
 
-    class currentGame(db_obj.Model):
-        __tablename__ = "current_game"
+#     class currentGame(db_obj.Model):
+#         __tablename__ = "current_game"
 
-        id = db_obj.Column(db_obj.Integer, primary_key=True)
-        daysAlive = db_obj.Column(db_obj.Integer, nullable=False)
-        happiness = db_obj.Column(db_obj.Integer, nullable=False)
-        saturation = db_obj.Column(db_obj.Integer, nullable=False)
-        fitness = db_obj.Column(db_obj.Integer, nullable=False)
-        sleep = db_obj.Column(db_obj.Integer, nullable=False)
+#         id = db_obj.Column(db_obj.Integer, primary_key=True)
+#         daysAlive = db_obj.Column(db_obj.Integer, nullable=False)
+#         happiness = db_obj.Column(db_obj.Integer, nullable=False)
+#         saturation = db_obj.Column(db_obj.Integer, nullable=False)
+#         fitness = db_obj.Column(db_obj.Integer, nullable=False)
+#         sleep = db_obj.Column(db_obj.Integer, nullable=False)
 
-    db_obj.create_all()  # Create all tables based on classes.
+#     db_obj.create_all()  # Create all tables based on classes.
 
-    # Seeding sample data for users and games that users have played
-    sample_users = [
-        User(realname="Sample user A", email="a@a.a", password="a"),
-        User(realname="Sample user B", email="b@b.b", password="b"),
-    ]
-    sample_games = [
-        PreviousGame(
-            userId=1, causeOfDeath="Died because of low Happiness", daysSurvived=27
-        ),
-        PreviousGame(
-            userId=1, causeOfDeath="Died because of low Saturation", daysSurvived=16
-        ),
-        PreviousGame(
-            userId=1, causeOfDeath="Died because of low Fitness", daysSurvived=4
-        ),
-        PreviousGame(
-            userId=1, causeOfDeath="Died because of low Sleep", daysSurvived=50
-        ),
-        PreviousGame(
-            userId=2, causeOfDeath="Died because of low Happiness", daysSurvived=25
-        ),
-        PreviousGame(
-            userId=2, causeOfDeath="Died because of low Saturation", daysSurvived=20
-        ),
-        PreviousGame(
-            userId=2, causeOfDeath="Died because of low Fitness", daysSurvived=8
-        ),
-        PreviousGame(
-            userId=2, causeOfDeath="Died because of low Sleep", daysSurvived=34
-        ),
-    ]
+#     # Seeding sample data for users and games that users have played
+#     sample_users = [
+#         User(realname="Sample user A", email="a@a.a", password="a"),
+#         User(realname="Sample user B", email="b@b.b", password="b"),
+#     ]
+#     sample_games = [
+#         PreviousGame(
+#             userId=1, causeOfDeath="Died because of low Happiness", daysSurvived=27
+#         ),
+#         PreviousGame(
+#             userId=1, causeOfDeath="Died because of low Saturation", daysSurvived=16
+#         ),
+#         PreviousGame(
+#             userId=1, causeOfDeath="Died because of low Fitness", daysSurvived=4
+#         ),
+#         PreviousGame(
+#             userId=1, causeOfDeath="Died because of low Sleep", daysSurvived=50
+#         ),
+#         PreviousGame(
+#             userId=2, causeOfDeath="Died because of low Happiness", daysSurvived=25
+#         ),
+#         PreviousGame(
+#             userId=2, causeOfDeath="Died because of low Saturation", daysSurvived=20
+#         ),
+#         PreviousGame(
+#             userId=2, causeOfDeath="Died because of low Fitness", daysSurvived=8
+#         ),
+#         PreviousGame(
+#             userId=2, causeOfDeath="Died because of low Sleep", daysSurvived=34
+#         ),
+#     ]
 
-    # Seed all sample user and game data. Commit to database
-    db_obj.session.add_all(sample_users)
-    db_obj.session.add_all(sample_games)
-    db_obj.session.commit()
+#     # Seed all sample user and game data. Commit to database
+#     db_obj.session.add_all(sample_users)
+#     db_obj.session.add_all(sample_games)
+#     db_obj.session.commit()
 
-    my_cursor.close()
+#     my_cursor.close()
 
 
-# Creating a cursor for use in other files.
-def create_cursor():
-    return_cursor = cnx.cursor()
-    return return_cursor
+# # Creating a cursor for use in other files.
+# def create_cursor():
+#     return_cursor = cnx.cursor()
+#     return return_cursor
